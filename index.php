0f586770ce475cf6ff9a01fddf65aecd810d139d
mariasolegallina <gallinamariasole@gmail.com>
2023-11-23 16:42:31 +0100

    add-index with form

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password generator</title>
+</head>
+<body>
+    
+<form action="" method="GET">
+    <label for="psw_length">Lunghezza:</label>
+    <input type="number" name="psw_length">
+    <button type="submit">Genera la password</button>
+</form>
+
+</body>
+</html>
